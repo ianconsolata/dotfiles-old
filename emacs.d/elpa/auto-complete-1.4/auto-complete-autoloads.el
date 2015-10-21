c2423aaeabb40d5871e7012056ea97d5a2f041ca
Ian Davis <jungziege@gmail.com>
2015-10-21 13:23:59 -0700

    Add .emacs.d

---
+++ emacs.d/elpa/auto-complete-1.4/auto-complete-autoloads.el
@@ -0,0 +1,18 @@
+;;; auto-complete-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("auto-complete-config.el" "auto-complete-pkg.el"
+;;;;;;  "auto-complete.el") (21337 29360 155737 0))
+
+;;;***
+
+(provide 'auto-complete-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; auto-complete-autoloads.el ends here
---
+++ emacs.d/elpa/goto-last-change-1.2/goto-last-change-autoloads.el
@@ -0,0 +1,18 @@
+;;; goto-last-change-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("goto-last-change-pkg.el" "goto-last-change.el")
+;;;;;;  (21648 52360 342832 0))
+
+;;;***
+
+(provide 'goto-last-change-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; goto-last-change-autoloads.el ends here
