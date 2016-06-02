f37ca20303627a32f0491ab7c4ba58c90232fb31
Ian Davis <jungziege@gmail.com>
2016-06-02 13:23:51 -0700

    Install macvim

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
+
+# install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install macvim --with-override-system-vim
+
 vim +PluginInstall +qall
