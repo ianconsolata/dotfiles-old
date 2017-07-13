93ac11c7fb9af61defdd2614ddf9430802f3003a
Ian Davis <jungziege@gmail.com>
2017-07-13 16:31:58 -0700

    Add setup.sh

--- setup.sh
+++ setup.sh
@@ -8,4 +8,5 @@ brew install macvim --with-override-system-vim
 
 git clone --depth=1 git@github.com:burke/matcher.git; cd matcher; make install; cd ..; rm -rf matcher
 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
