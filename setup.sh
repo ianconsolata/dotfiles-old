3260127f90d58bff8aa5abca5d99db380c170316
Ian Davis <jungziege@gmail.com>
2018-09-05 16:43:02 -0700

    Add brew bundle

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ done
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install macvim --with-override-system-vim
+brew bundle
 
 git clone --depth=1 git@github.com:burke/matcher.git; cd matcher; make install; cd ..; rm -rf matcher
 
